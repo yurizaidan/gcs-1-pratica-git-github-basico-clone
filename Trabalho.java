2b8944a0c292f936cbfa2f995577b2d25f42922b
Artur Lemos Pereira <abui.lepereira@gmail.com>
2023-08-25 13:45:22 -0300

    Metodos A,B,D e E Feitos

--- Trabalho.java
+++ Trabalho.java
@@ -1,29 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays; 
-import java.util.List;
-import java.util.stream.Collectors;
 
-public class main {
-
-    //Método que retorna quantas ocorrências de um elemento estão na lista
-    public static int nOcorrencias (ArrayList<Integer> list, int target){
-        int count = 0; 
-        for (int e:numeros){
-            if (target=e){
-            count++;
-            }
-        }
-        return count; 
-    }
-    
-    //Método que retorna uma lista de elementos repetidos de l
-    public static List<Integer> listRepeat (List<Integer> list){
-        Set<Integer> elements = new HashSet<Integer>();
-        return list.stream().filter(n -> !elements.add(n)).collect(Collectors.toList());
-    
-    }
-
-public static void main(String Args[]){
+public class Trabalho {
+    public static void main(String Args[]){
 ArrayList<Integer> listWithRepeats = new ArrayList<>();
 listWithRepeats.add(1);
 listWithRepeats.add(5);
@@ -32,25 +10,11 @@ listWithRepeats.add(5);
 
 ArrayList<Integer> listWithoutRepeats = new ArrayList<>();
 listWithoutRepeats.add(5);
-listWhithoutRepeats.add(6);
+listWithoutRepeats.add(6);
 listWithoutRepeats.add(7);
 
-System.out.println(hasRepeat(listWithRepeats));
-System.out.println(hasRepeat(listWithoutRepeats));
-
-ArrayList<Integer> listWithRepeats = new ArrayList<>();
-listWithRepeats.add(1);
-listWithRepeats.add(5);
-listWithRepeats.add(10);
-listWithRepeats.add(5);
-
-ArrayList<Integer> listWithoutRepeats = new ArrayList<>();
-listWithoutRepeats.add(5);
-listWhithoutRepeats.add(6);
-listWithoutRepeats.add(7);
-
-System.out.println(elementoRepetido(listWithRepeats));
-System.out.println(elementoRepetido(listWithoutRepeats));
+System.out.println(elementosRepetidos(listWithRepeats));
+System.out.println(elementosRepetidos(listWithoutRepeats));
 
  ArrayList<Integer> lista1 = new ArrayList<>();
         lista1.add(1);
@@ -68,9 +32,8 @@ System.out.println(elementoRepetido(listWithoutRepeats));
 
 
 }
-}
 
-static boolean elementoRepetido(ArrayList<Integer> l) {
+static boolean elementosRepetidos(ArrayList<Integer> l) {
         for (int i = 0; i < l.size(); i++) {
             for (int j = i + 1; j < l.size(); j++) {
                 if (l.get(i).equals(l.get(j))) {
@@ -80,21 +43,17 @@ static boolean elementoRepetido(ArrayList<Integer> l) {
         }
         return false;
     }
-}
 
 
 
-public class ExemploUniao {
-    public static ArrayList<Integer> uniao(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+public static ArrayList<Integer> uniao(ArrayList<Integer> l1, ArrayList<Integer> l2) {
         ArrayList<Integer> resultado = new ArrayList<>();
-
 
         for (Integer elemento : l1) {
             if (!resultado.contains(elemento)) {
                 resultado.add(elemento);
             }
         }
-
 
         for (Integer elemento : l2) {
             if (!resultado.contains(elemento)) {
@@ -103,4 +62,6 @@ public class ExemploUniao {
         }
 
         return resultado;
+    }
+
 }
